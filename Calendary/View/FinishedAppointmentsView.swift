8f0b8d10fa6be5ef8297094abd0d695de94a0893
felipesantos22 <felipevs2018@gmail.com>
2025-12-15 22:48:25 -0300

    insert delete

--- Calendary/View/FinishedAppointmentsView.swift
+++ Calendary/View/FinishedAppointmentsView.swift
@@ -10,29 +10,50 @@ import SwiftData
 
 struct FinishedAppointmentsView: View {
     
+    @Environment(\.modelContext) private var modelContext
+    
     @Query(
         filter: #Predicate<Appointment> { $0.isDone },
         sort: \Appointment.date
     )
-    
     private var appointments: [Appointment]
     
     var body: some View {
-        List {
-            ForEach(appointments) { appointment in
-                VStack(alignment: .leading) {
-                    Text(appointment.patientName)
-                        .font(.headline)
-                    
-                    Text(
-                        appointment.date.formatted(
-                            date: .abbreviated,
-                            time: .shortened
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.colorOne, .colorTwo]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            List {
+                ForEach(appointments) { appointment in
+                    VStack(alignment: .leading) {
+                        Text(appointment.patientName)
+                            .font(.headline)
+                        
+                        Text(
+                            appointment.date.formatted(
+                                date: .abbreviated,
+                                time: .shortened
+                            )
                         )
-                    )
-                    .foregroundColor(.gray)
+                        .foregroundColor(.gray)
+                    }
                 }
+                .onDelete(perform: deleteAppointments) // 👈 aqui
+                .listRowBackground(Color.clear)
             }
+            .scrollContentBackground(.hidden)
+        }
+    }
+    
+    // MARK: - Delete
+    private func deleteAppointments(at offsets: IndexSet) {
+        for index in offsets {
+            let appointment = appointments[index]
+            modelContext.delete(appointment)
         }
     }
     
